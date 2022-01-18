e1fa87a817d53b1a5dbe399a1e7d5c1b0f9a7e6e
xzzhy2010@163.com <zyh123>
2022-01-18 17:17:58 +0800

    更新1.0.5

--- app/src/main/java/com/drumbeat/filepicker/MainActivity.java
+++ app/src/main/java/com/drumbeat/filepicker/MainActivity.java
@@ -1,16 +1,18 @@
 package com.drumbeat.filepicker;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.ess.filepicker.FilePicker;
 import com.ess.filepicker.model.EssFile;
 import com.ess.filepicker.util.Const;
+import com.ess.filepicker.util.LogUtils;
 
 import java.util.ArrayList;
 
@@ -34,17 +36,48 @@ public class MainActivity extends AppCompatActivity {
                         .setFileTypes("pdf")
                         .requestCode(1)
                         .start();
+//                FindAllFiles.folderMethod1(Environment.getExternalStorageDirectory().getPath());
+//                FindAllFiles.folder(MainActivity.this);
+//                String path = "/storage/emulated/0/Android/data/";
+//                String path = "/storage/emulated/0/Android/data/com.tencent.mobileqq/Tencent";
+//                FileUriUtils.startFor(path, MainActivity.this, 2);
+//                if (haspermission) {
+//                    DocumentFile documentFile = DocumentFile.fromTreeUri(MainActivity.this, Uri.parse(FileUriUtils.changeToUri3(path)));
+//                    getFiles(documentFile);
+//                }
+
             }
         });
 
     }
 
+    boolean haspermission;
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 1 && resultCode == RESULT_OK) {
+            StringBuilder stringBuilder = new StringBuilder();
             ArrayList<EssFile> essFileList = data.getParcelableArrayListExtra(Const.EXTRA_RESULT_SELECTION);
-            pdf.setText(essFileList.get(0).getName());
+            for (int i = 0; i < essFileList.size(); i++) {
+                stringBuilder.append(essFileList.get(i).getName())
+                        .append(",");
+            }
+            pdf.setText(stringBuilder.toString());
+        }
+        if (requestCode == 2) {
+            Uri uri;
+            if (data == null) {
+                return;
+            }
+            if (requestCode == 2 && (uri = data.getData()) != null) {
+                getContentResolver().takePersistableUriPermission(uri, data.getFlags() & (Intent.FLAG_GRANT_READ_URI_PERMISSION
+                        | Intent.FLAG_GRANT_WRITE_URI_PERMISSION));//关键是这里，这个就是保存这个目录的访问权限
+
+                haspermission = true;
+
+            }
         }
     }
+
 }
